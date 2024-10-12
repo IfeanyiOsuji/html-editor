a15d8a3b97adaa0f4227259be5fc85e839ad6a7a
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-10-12 12:15:13 +0100

    created view and controller

---
+++ src/View.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private  Controller controller;
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+    public Controller getController() {
+        return controller;
+    }
+    public void init() {
+    }
+}
