07971227e2726f0045cb0d6cb2814232db26d14d
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-10-12 12:50:20 +0100

    added TabbedPaneChangeLister and Exception handler classes

--- src/view/View.java
+++ src/view/View.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.Controller;
+import listeners.FrameListener;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -23,8 +24,19 @@ public class View extends JFrame implements ActionListener {
         return controller;
     }
     public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        this.setVisible(true);
     }
     public void exit(){
         controller.exit();
+    }
+    public void initMenuBar(){
+
+    }
+    public void initEditor(){}
+    public void initGui(){}
+
+    public void selectedTabChanged() {
     }
 }
