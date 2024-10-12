d722e7c2e37093f06237d84f6d752a362cb48eb8
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-10-12 15:01:11 +0100

    implemented actions and listeners

---
+++ src/actions/StrikethroughAction.java
@@ -0,0 +1,15 @@
+package actions;
+
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class StrikethroughAction extends StyledEditorKit.StyledTextAction{
+    public StrikethroughAction() {
+        super(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
